8da62aebfa75d3f948f63ff1003686a7e6659020
Rafaeltans <rafaelivan797@gmail.com>
2025-06-19 15:30:44 +0700

    Finish projek

---
+++ script.js
@@ -0,0 +1,28 @@
+// script.js
+let display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function persen(){
+  let current = eval(display.value);
+  display.value = current/100
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+//   try {
+//     display.value = eval(display.value);
+//   } catch (error) {
+//     alert('Input tidak valid!');
+//   }
+    display.value=eval(display.value)
+}
